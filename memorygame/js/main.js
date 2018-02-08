ef33f1af6a048694cf554a8a8fc8c25987e78a06
Sam Mintz <sammintz@Sams-MacBook-Air.local>
2018-02-08 15:40:17 -0600

    created game logic

---
+++ memorygame/js/main.js
@@ -0,0 +1,9 @@
+
+
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
