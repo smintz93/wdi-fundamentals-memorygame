8eb82926e8ac6d558468858eed11aee7b39f8b07
Sam Mintz <sammintz@Sams-MacBook-Air.local>
2018-02-14 12:09:24 -0600

    created game logic

--- memorygame/js/main.js
+++ memorygame/js/main.js
@@ -1,31 +1,34 @@
 
 
+
 var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = []
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
 
+var checkforMatch = function() {
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-
-
-if (cardsInPlay.length === 2)
-	
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
+		alert("You found a match!");
 	} else {
-		alert('Sorry, try again');
+		alert("Sorry, try again");
 	}
+};
 
 
+		
+
+var flipCard = function(cardId) {
 	
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+};
+
+flipCard(0);
+flipCard(2);
+
+checkforMatch();
 
 
 
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
